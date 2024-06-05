cdbf982809d21c910fac7f2a83088b884b354050
Miguel Fernandes <migamado01@gmail.com>
2024-06-05 10:14:12 -0400

    optional route parameters

---
+++ src/routes/marketing/[[lang]]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import {page} from "$app/stores";
+
+    const greetings = {
+        en: 'Hello',
+        es: 'Hola',
+        fr: 'Bonjour',
+    }
+
+    const { lang } = $page.params;
+    const greeting: string = greetings[lang] ?? greetings.en;
+</script>
+
+<h1>{greeting}</h1>
