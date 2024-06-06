be366d7c271c2b7cf509d51312ac86ca76a2f40a
Miguel Fernandes <migamado01@gmail.com>
2024-06-06 11:04:54 -0400

    extract header and footer into separate svelte components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,8 @@
-<header class="layout-header">Header</header>
-<slot />    <!-- This is where the content of the route will be rendered -->
-<footer class="layout-footer">Footer</footer>
+<script>
+    import Header from './header.svelte';
+    import Footer from './footer.svelte';
+</script>
 
-<style>
-    .layout-header {
-        background-color: #333;
-        color: #f4a261;
-        font-size: 30px;
-        text-align: center; 
-        padding: 10px;
-    }
-    .layout-footer {
-        background-color: #333;
-        color: #f4a261;
-        font-size: 30px;
-        text-align: center;
-        padding: 10px;
-    }
-</style>
+<Header />
+<slot />    <!-- This is where the content of the route will be rendered -->
+<Footer />
---
+++ src/routes/footer.svelte
@@ -0,0 +1,11 @@
+<footer class="layout-footer">Footer</footer>
+
+<style> 
+    .layout-footer {
+        background-color: #333;
+        color: #f4a261;
+        font-size: 30px;
+        text-align: center;
+        padding: 10px;
+    }
+</style>
