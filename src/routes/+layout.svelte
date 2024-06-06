bdd88e1a3ea4941b0971875419b7980af8b9685e
Miguel Fernandes <migamado01@gmail.com>
2024-06-06 10:50:40 -0400

    basic layouts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<header class="layout-header">Header</header>
+<slot />    <!-- This is where the content of the route will be rendered -->
+<footer class="layout-footer">Footer</footer>
+
+<style>
+    .layout-header {
+        background-color: #333;
+        color: #f4a261;
+        font-size: 30px;
+        text-align: center; 
+        padding: 10px;
+    }
+    .layout-footer {
+        background-color: #333;
+        color: #f4a261;
+        font-size: 30px;
+        text-align: center;
+        padding: 10px;
+    }
+</style>
